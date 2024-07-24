3b0edb11514b373c237b71ffdd10b8118acf560a
Shuashuaa <jovijoshuatania027@gmail.com>
2024-07-24 21:23:15 +0800

    072424-exporttsvuecomponent

---
+++ src/components/Nav.vue
@@ -0,0 +1,22 @@
+<template>
+    <h1>this is Nav!</h1>
+    <div v-for="champs in list">
+        <p>{{ champs.champion }}</p>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue'
+
+export default defineComponent({
+    setup() {
+        const list = ref([
+            { id: 1, champion: 'maokai'},
+            { id: 2, champion: 'lee sin'},
+            { id: 3, champion: 'asdasd'}
+        ]);
+
+        return { list }
+    },
+})
+</script>
