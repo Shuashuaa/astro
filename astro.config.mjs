a8447956bb505d95f5745e121e69d6e02f77af64
Shuashuaa <jovijoshuatania027@gmail.com>
2024-08-06 21:20:47 +0800

    080624-v.0.1.2

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,10 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [vue(), tailwind()]
+  integrations: [
+    vue(), 
+    tailwind({
+      applyBaseStyles: false
+    }),
+  ]
 });
